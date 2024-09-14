76d87370cff2ebf265a24cdfb45ecbbc3b94d185
shamilfrontend <shoma.alisultanov@yandex.ru>
2024-09-14 14:52:25 +0300

    feat: start

---
+++ data/profileInfo.js
@@ -0,0 +1,7 @@
+export const profileInfo = {
+  name: 'Shamil Alisultanov',
+  title: 'Frontend Development',
+  imageSrcOne: '/assets/img/parsonal-info/avatar.png',
+  imageSrcTwo: '/assets/img/parsonal-info/parson-img-3.png',
+  imageSrcThree: '/assets/img/parsonal-info/parson-img-2.png',
+}
